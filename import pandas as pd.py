6f9b4d95bf50e06589003827befe7e8f1fc93735
rodnameshit <muhammedahmed1542005@gmail.com>
2025-04-22 12:26:44 +0100

    added the correct class

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -3,7 +3,11 @@ import time
 from abc import ABC, abstractmethod
 import matplotlib.pyplot as plt
 
-
+# ------------------ Abstract Classes ------------------
+class SortAlgorithm(ABC):
+    @abstractmethod
+    def sort(self, data):
+        pass
 
 # ------------------ Data Model ------------------
 class WorkoutSession:
